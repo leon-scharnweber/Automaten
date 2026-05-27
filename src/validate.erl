494bce2dddaf85b818f79216217d52f6e016eed9
Leon Scharnweber <git.hub.leon@gmail.com>
2026-05-27 10:53:14 +0200

    added simple alphabet validation

--- src/validate.erl
+++ src/validate.erl
@@ -1,4 +1,14 @@
 -module(validate).
 -export([validate/1]).
 
-validate(DEA) -> ok.
+validate({States, Alphabet, Transitions, StartState}) -> validateAlphabet(Alphabet).
+
+validateAlphabet(Alphabet) -> validateAlphabet(Alphabet, []).
+
+validateAlphabet([], _Seen) ->
+    ok;
+validateAlphabet([CurChar | RestChar], Seen) ->
+    case lists:member(CurChar, Seen) of
+        false -> validateAlphabet(RestChar, [CurChar | Seen]);
+        true -> {error, CurChar}
+    end.
